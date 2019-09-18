d7099c7e8e04e2505b96e469c28c1bafe05e7f7d
Eric Hubbell <eric.hubbell@gmail.com>
2019-09-18 15:07:56 -0600

    add abortcontroller-polyfill

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "fastboot",
-    script: "/var/app/current/fastboot-app-server/server.js",
+    script: "/var/app/fastboot-app-server/server.js",
     log_date_format: 'MM-DD-YYYY HH:mm Z',
     max_memory_restart: '100M',
     instances: 'max',
