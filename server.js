396f2919fe8934658c16cd6ed3ad34a411ea9568
Eric Hubbell <eric.hubbell@gmail.com>
2020-10-25 13:54:12 -0600

    update server.js for local project

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
 require('dotenv').config();
-
-const S3Downloader = require('fastboot-s3-downloader');
-const S3Notifier = require('fastboot-s3-notifier');
 const FastBootAppServer = require('fastboot-app-server');
 
-const S3_BUCKET = process.env.FASTBOOT_S3_BUCKET;
-const S3_KEY = process.env.FASTBOOT_S3_KEY;
-const AWS_KEY = process.env.AWS_KEY;
-const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-
-let downloader = new S3Downloader({
-  bucket: S3_BUCKET,
-  key: S3_KEY,
-  accessKeyId: AWS_KEY,
-  secretAccessKey: AWS_SECRET_KEY
-});
-
-let notifier = new S3Notifier({
-  bucket: S3_BUCKET,
-  key: S3_KEY,
-  accessKeyId: AWS_KEY,
-  secretAccessKey: AWS_SECRET_KEY
-});
-
 let server = new FastBootAppServer({
-  downloader: downloader,
-  notifier: notifier,
+  distPath: '../bb-web/dist',
   gzip: true,
   host: '0.0.0.0',
   port: 4000,
